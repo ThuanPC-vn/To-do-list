ed59de43483687c0115ea21d2a9a3c83d32acee0
ThuanPC-Linux <thuanmclaren@gmail.com>
2024-07-17 22:22:27 +0700

    update js-list-arr by github account ThuanPC-linux

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ function addTask(){
     if (task){
         createElementTask(task);
         inputField.value = '';
+        saveTask();
     }
     else{
         alert("Plesea write a task you want to do.");
@@ -26,7 +27,7 @@ function createElementTask(task){
     listTasks.appendChild(liTask);
 
     const pTask = document.createElement('p');
-    pTask.innerText = task;
+    pTask.textContent = task;
     pTask.className = "name__task";
     liTask.appendChild(pTask);
 
@@ -34,12 +35,55 @@ function createElementTask(task){
     divAction.className = "action__task";
     liTask.appendChild(divAction);
 
-    const inputCheckbox = document.createElement('input');
-    inputCheckbox.className = "checkbox__task";
-    inputCheckbox.setAttribute("type", "checkbox");
-    divAction.appendChild(inputCheckbox);
+    const iconCheckbox = document.createElement('i');
+    iconCheckbox.classList.add('bx', 'bx-checkbox-square', 'bx-md', 'bx__noCheck');
+    iconCheckbox.setAttribute("id", "iNoCheck")
+    divAction.appendChild(iconCheckbox);
 
     const iconDelete = document.createElement('i');
-    iconDelete.classList.add('bx', 'bxs-trash', 'bx-md');
+    iconDelete.classList.add('bx', 'bxs-trash', 'bx-sm', 'bx__trash');
     divAction.appendChild(iconDelete);
+
+    iconCheckbox.addEventListener('click',() => {
+        if (iconCheckbox.id == "iNoCheck"){
+            pTask.style.textDecoration = "line-through";
+            pTask.style.filter = "opacity(50%)";
+
+            iconCheckbox.classList.remove('bx', 'bx-checkbox-square', 'bx-md', 'bx__noCheck');
+            iconCheckbox.classList.add('bx', 'bxs-check-square', 'bx-sm', 'bx__Checked');
+            iconCheckbox.setAttribute("id", "Checked");
+        }
+        else{
+            pTask.style.textDecoration = "none";
+            pTask.style.filter = "opacity(100%)";
+
+            iconCheckbox.classList.remove('bx', 'bxs-check-square', 'bx-sm', 'bx__Checked');
+            iconCheckbox.classList.add('bx', 'bx-checkbox-square', 'bx-md', 'bx__noCheck');
+            iconCheckbox.setAttribute("id", "iNoCheck");
+        }
+    })
+}
+
+function saveTask(){
+
+    let taskContent = document.getElementById('taskContent');
+    let tasks = [];
+    let idTask = 0;
+
+    taskContent.querySelectorAll('p.name__task').forEach(function(item){
+         
+        let nameTaskValue = item.textContent.trim();
+        var ojectTask = new Object();
+
+        ojectTask.id = idTask;
+        ojectTask.nameTask = nameTaskValue;
+
+        tasks.push(ojectTask)//<= push a Object right here
+
+        idTask++;
+    });
+    
+    localStorage.setItem('taskList', JSON.stringify(tasks));
+    
+    console.log(tasks);
 }
