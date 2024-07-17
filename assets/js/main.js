67f447a88f46afc37d95b256e258f3725d9fd453
ThuanPC-Linux <thuanmclaren@gmail.com>
2024-07-17 15:24:33 +0700

    update js  by github account ThuanPC-linux

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,45 @@
-// let addTodo = document.getElementById('AddtaskButton');
+let addTodo = document.getElementById('addTaskButton');
 
-// let listTasks = document.getElementById('list-tasks');
-// let inputField = document.getElementById('input-field');
-
-
-// function TasksFun(){
-
-//     if (!inputField.value) {
-//         alert("Please fill out the task");
-//         return;
-//     }
-
-//     var divBG = document.createElement('div');
-//     divBG.classList.add('work__bg');
-//     listTasks.appendChild(divBG);
-
-//     var divContent = document.createElement('div');
-//     divContent.classList.add('works__content');
-//     divBG.appendChild(divContent);
-
-
-//     var lab = document.createElement('label');
-//     lab.classList.add('name__works');
-//     lab.innerText = inputField.value;
-//     divContent.appendChild(lab);
-
-//     var divAction = document.createElement('div');
-//     divAction.classList.add('action__buttons');   
-//     divContent.appendChild(divAction);
-
-//     var ipAction = document.createElement('input');
-//     ipAction.classList.add('checkbox__work');
-//     ipAction.setAttribute("type", "checkbox")
-//     ipAction.setAttribute("id", "compeleteCheck")
-//     divAction.appendChild(ipAction);
-
-//     var iAction = document.createElement('i');
-//     iAction.classList.add('bx');
-//     iAction.classList.add('bxs-trash');
-//     iAction.setAttribute("id", "deleTask")
-//     divAction.appendChild(iAction);
-
-    
-//     ipAction.addEventListener('click',() => {
-//         if (ipAction.checked == true){ 
-//             lab.style.textDecoration = "line-through";
-//             lab.style.filter = "opacity(50%)";
-//         }
-//         else{
-//             lab.style.textDecoration = "none";
-//             lab.style.filter = "opacity(100%)";
-//         }
-//     })
-    
-
-//     iAction.addEventListener('click',() => {
-//         listTasks.removeChild(divBG);
-//     })
-
-//     inputField.value = "";
-// };
-
-
-// addTodo.addEventListener('click',TasksFun);
+let listTasks = document.getElementById('list-tasks');
+let inputField = document.getElementById('input-field');
 
 
 
+function addTask(){
+
+    const task = inputField.value;
+    if (task){
+        createElementTask(task);
+        inputField.value = '';
+    }
+    else{
+        alert("Plesea write a task you want to do.");
+    }
+}
+
+addTodo.addEventListener('click',addTask)
+
+
+function createElementTask(task){
+    const liTask = document.createElement('li');
+    liTask.className = "task__content";
+    listTasks.appendChild(liTask);
+
+    const pTask = document.createElement('p');
+    pTask.innerText = task;
+    pTask.className = "name__task";
+    liTask.appendChild(pTask);
+
+    const divAction = document.createElement('div');
+    divAction.className = "action__task";
+    liTask.appendChild(divAction);
+
+    const inputCheckbox = document.createElement('input');
+    inputCheckbox.className = "checkbox__task";
+    inputCheckbox.setAttribute("type", "checkbox");
+    divAction.appendChild(inputCheckbox);
+
+    const iconDelete = document.createElement('i');
+    iconDelete.classList.add('bx', 'bxs-trash', 'bx-md');
+    divAction.appendChild(iconDelete);
+}
