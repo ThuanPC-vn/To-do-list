d2f107162f7bb6cc6e2593ba3187a9bea995b0ac
ThuanPC-Linux <thuanmclaren@gmail.com>
2024-07-21 13:47:21 +0700

    update js - remake funtion deleteTask & loadTask - done

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,33 +83,22 @@ function createElementTask(idTask,task){
     
 }
 
-function reloadAfterDelete(todoData){
-
-    const listElement = listTasks.querySelectorAll('li#taskContent');
-    for(let i=0; i<listElement.length ;i++){
-        listElement[i].parentNode.removeChild(listElement[i]);
-        // console.log(listElement[i]);
-    }
-
-    todoData.forEach((e)=>{
-        createElementTask(e.idTask, e.nameTaskValue);
-    })
-}
-
 
 /*====================== DELETE TASK ========================= */
 function deleteTask(idTask){
-    // const li = document.getElementById('taskContent');
+
     todoData = JSON.parse(localStorage.getItem('taskList'));
 
-    for(var i=0; i < todoData.length; i++){
+    const listElement = listTasks.querySelectorAll('li#taskContent');
+
+    for(let i=0; i < todoData.length; i++){
         if (todoData[i].idTask == idTask){
             todoData.splice(i, 1);
+            listElement[i].parentNode.removeChild(listElement[i]);
             localStorage.setItem('taskList', JSON.stringify(todoData));
         }
     }
 
-    reloadAfterDelete(todoData);
 }
 
 
@@ -130,32 +119,23 @@ function checkArr(tasksCheck){
 //Funtion to find id largest
 function checkID(todo,idMax){
     todo = JSON.parse(localStorage.getItem('taskList'));
-    if(!todo){
+    if(!todo || todo.length == 0 || todo === undefined){
         idTask=0;
         return idTask;
     }
     else{
         let maxID = 0
         todo.forEach((item)=>{
-            console.log(item.idTask);
             if(item.idTask > maxID){
                 maxID = item.idTask; 
             }
         })
-        console.log(maxID);
         idMax = maxID;
         return idMax+1;
     }
 }
 
-/*========== RENDER TASKS FROM LISH TASK SAVED INTO ARRAY INSIDE localStorage ========================= */
-function loadTask(){
-    const todo = JSON.parse(localStorage.getItem('taskList')) || [];
-    
-    todo.forEach((element)=>{
-        createElementTask(element.id,element.nameTaskValue);
-    });
-}
+
 
 /*============= SAVE TASK INTO OBJECT & PUSH THE ARRAY INSIDE localStorage =================== */
 function saveTask(){
@@ -166,7 +146,6 @@ function saveTask(){
 
     //Call funtion to check Array
     tasks = checkArr(tasksCheck);
-    console.log(typeof tasks);
 
     //Call funtion to find id largest and 
     idTask = checkID(tasks, idCheck);
@@ -191,7 +170,67 @@ function saveTask(){
 }
 
 
+/*========== RENDER TASKS FROM LISH TASK SAVED INTO ARRAY INSIDE localStorage ========================= */
+function loadTask(){
+    const todo = JSON.parse(localStorage.getItem('taskList')) || [];
+    
+    todo.forEach((element)=>{
+        /*====================== CREAT ELEMENT FOR TASK========================= */
+        const liTask = document.createElement('li');
+        liTask.className = "task__content";
+        liTask.setAttribute("id", "taskContent");
+        listTasks.appendChild(liTask);
 
+        const todoItem = `<p class="name__task" id="nameTask">${element.nameTaskValue}</p>
+                          <div class="action__task">
+                              <i class='bx bxs-check-square bx-sm bx__Checked' style="display: none" id="iChecked"></i>
+                              <i class='bx bx-checkbox-square bx-md bx__noCheck' id="iNoCheck"></i>
+                              <i class='bx bxs-trash bx-sm bx__trash' id="deleTask" onclick="deleteTask(${element.idTask})"></i>
+                          </div>`
+        liTask.innerHTML = todoItem;
+
+            /*====================== CHECKBOX ACTION ========================= */
+        let iconCheckbox = liTask.querySelector('i#iNoCheck');
+        let taskName = liTask.querySelector('p#nameTask');
+
+        iconCheckbox.addEventListener('click',() => {
+            if (iconCheckbox.id == "iNoCheck"){
+                taskName.style.textDecoration = "line-through";
+                taskName.style.filter = "opacity(50%)";
+
+                iconCheckbox.classList.remove(
+                    'bx',
+                    'bx-checkbox-square',
+                    'bx-md',
+                    'bx__noCheck');
+
+                iconCheckbox.classList.add('bx',
+                    'bxs-check-square',
+                    'bx-sm', 
+                    'bx__Checked');
+
+                iconCheckbox.setAttribute("id","Checked");
+            }
+            else{
+                taskName.style.textDecoration = "none";
+                taskName.style.filter = "opacity(100%)";
+
+                iconCheckbox.classList.remove(
+                    'bx', 
+                    'bxs-check-square', 
+                    'bx-sm', 
+                    'bx__Checked');
+
+                iconCheckbox.classList.add('bx', 
+                    'bx-checkbox-square', 
+                    'bx-md', 
+                    'bx__noCheck');
+
+                iconCheckbox.setAttribute("id", "iNoCheck");
+            }
+        })
+    });
+}
 
 
 
